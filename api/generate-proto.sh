d1764aaf500bae06e10211a2f8f745c9468af6b9
Joe Lombrozo <joe@djeebus.net>
2016-02-16 00:44:54 -0800

    Update to cast_channel.proto, r269445

--- api/generate-proto.sh
+++ api/generate-proto.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
+# cast_channel.proto comes from /src/extensions/common/api/cast_channel/cast_channel.proto
+
+# to run the command on ubuntu 15.10,
+# apt-get install protobuf-compiler golang-gogoprotobuf-dev
 protoc --gogo_out=. cast_channel.proto
